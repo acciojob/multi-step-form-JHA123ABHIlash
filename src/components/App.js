1d480e53413b3c8af3c4888762b0901277bc6c36
Abhilash Jha <abhilashjha121@gmail.com>
2026-05-03 15:49:57 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Step from "./Step";
 
 const App = () => {
+  const [step, setStep] = useState(1);
+  const [formdata, setFormdata] = useState({
+    fname: "",
+    lname: "",
+    model: "",
+    brand: "",
+    card_info: "",
+    card_exp: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormdata((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function handleNext() {
+    setStep((prev) => prev + 1);
+  }
+
+  function handlePrev() {
+    setStep((prev) => prev - 1);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formdata);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Step
+        step={step}
+        formdata={formdata}
+        handleChange={handleChange}
+        handleNext={handleNext}
+        handlePrev={handlePrev}
+        handleSubmit={handleSubmit}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Step.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Step({step,formdata,handleChange,handleNext,handlePrev,handleSubmit}) {
+  
+        if(step==1){
+           return (
+            <div>
+                <label htmlFor='fname'>First Name:</label>
+                <input type='text' id='fname' name='fname' value={formdata.fname} onChange={handleChange}/>
+                 <label htmlFor='lname'>Last Name:</label>
+                <input type='text' id='lname' name='lname' value={formdata.lname} onChange={handleChange}/>
+                <button onClick={handleNext}>Next</button>
+            </div>
+           ) 
+        }else if(step==2){
+            return (
+            <div>
+                <label htmlFor='brand'>Brand:</label>
+                <input type='text' id='brand' name='brand' value={formdata.brand} onChange={handleChange}/>
+                 <label htmlFor='model'>Model:</label>
+                <input type='text' id='model' name='model' value={formdata.model} onChange={handleChange}/>
+                <button onClick={handlePrev}>Previous</button>
+                <button onClick={handleNext}>Next</button>
+            </div>
+           ) 
+        }else if(step==3){
+            return (
+            <div>
+                <label htmlFor='card_info'>Credit Card No:</label>
+                <input type='text' id='card_info'name='card_info' value={formdata.card_info} onChange={handleChange} minLength={12}/>
+                 <label htmlFor='expiry_date'>Card Exp:</label>
+                <input type='text' id="expiry_date" name='card_exp' value={formdata.card_exp} onChange={handleChange}/>
+                <button onClick={handlePrev}>Previous</button>
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+           ) 
+        }
+        
+  
+}
